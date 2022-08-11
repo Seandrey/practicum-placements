ebd037b353ba06cc3153f2a53ce477d89e5a59ff
SimplyTheOther <simplytheother@gmail.com>
2022-08-11 12:16:48 +0800

    Added more info for next steps on API

--- app/qualtrics_import.py
+++ app/qualtrics_import.py
@@ -22,6 +22,7 @@ def get_something():
     
     survey_id = "Sv_someweirdhexurlthing"
     data_centre = "co1" # unsure what this is
+    # can get IDs from here apparently: https://api.qualtrics.com/ZG9jOjg3NjYzNQ-finding-your-qualtrics-i-ds 
 
     # set static params
     request_check_progress = 0.0
@@ -35,7 +36,9 @@ def get_something():
     # 1: create data export
     data = {
         "format": "csv",
-        "seenUnansweredRecode": 2 # unsure what this does
+        "seenUnansweredRecode": 2, # converts questions that weren't answered to "2", can't be used for JSON
+        "startDate": "2019-04-30T07:31:43Z",
+        "useLabels": True # shows text shown to user instead of internal numbers
     }
 
     # TODO: surely we don't need another external library for HTTP requests
@@ -80,3 +83,42 @@ def get_something():
     # 4: unzip file
     zipfile.ZipFile(io.BytesIo(request_download.content)).extractall("MyQualtricsDownload")
     print("complete")
+
+# JSON format (apparently):
+"""
+{
+    "responses": [{
+        "responseId": "horrible_hex_number",
+        "values": {
+            "startDate": "2019-05-02T14:06:49Z",
+            "endDate": "2019-05-02T14:06:58Z",
+            "status": 0,
+            "ipAddress": "24.197.127.176",
+            "progress": 100,
+            "duration": 9,
+            "finished": 1,
+            "recordedDate": "2019-05-02T14:06:59.208Z",
+            "_recordId": "R_1Cx8FIukucgqM94",
+            "locationLatitude": "34.8307952880859375",
+            "locationLongitude": "-82.35070037841796875",
+            "distributionChannel": "anonymous",
+            "userLanguage": "EN", 
+            "QID2": 2,
+            "QID3": 2
+        },
+        "labels": {
+            "status": "IP Address",
+            "finished": "True",
+            "QID2": "fair",
+            "QID3": "maybe"
+        },
+        "displayedFields": ["QID1", "QID3", "QID2"],
+        "displayedValues: {
+            "QID1": [1, 2],
+            "QID3": [1, 2, 3],
+            "QID2": [1, 2, 3]
+        }
+    }]
+}
+
+"""
